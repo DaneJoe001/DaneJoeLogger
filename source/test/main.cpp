fde133c9cc08e8ddb138e0986ba9a32b3849efcb
DaneJoe001 <2845547447@qq.com>
2025-08-23 09:53:53 +0800

    commit01 : 完成日志抽象接口设计，日志管理器设计，完成自定义异步日志实现

---
+++ source/test/main.cpp
@@ -0,0 +1,23 @@
+#include "log/danejoe_logger.hpp"
+#include "log/manage_logger.hpp"
+#include <thread>
+
+int main()
+{
+    DaneJoe::DaneJoeLogger::LoggerConfig log_config;
+    log_config.log_name = "DaneJoe";
+    DaneJoe::ManageLogger& manage_logger = DaneJoe::ManageLogger::get_instance();
+    manage_logger.add_logger("default", log_config);
+
+    auto logger = manage_logger.get_logger("default");
+
+
+
+    logger->trace(std::string("test"), std::string("E:\\personal_code\\code_cpp_library\\library_danejoe_log\\source\\test\\main.cpp"), std::string(__FUNCTION__), 9, "test {}", 7);
+
+    DANEJOE_LOG_TRACE("default", "test", "测试 {}", 7);
+
+    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    return 0;
+}
